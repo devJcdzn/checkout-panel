712caa18c99e7701733885c46f0fd83953b3ad89
JcDev <lopesjean81@gmail.com>
2024-11-14 15:54:33 -0300

    test(middleware)

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const hostname = request.nextUrl.hostname;
+
+  // Redirecionar para o painel administrativo se for `admin.seudominio.com`
+  if (hostname === "app.checkseguro.pro" || "checkseguro.pro") {
+    return NextResponse.rewrite(new URL("/panel", request.url));
+  }
+
+  // Redirecionar para a área pública para `app.seudominio.com`
+  if (hostname === "pay.checkseguro.pro") {
+    return NextResponse.rewrite(new URL("/", request.url));
+  }
+
+  // Redirecionar para a página pública padrão para outros casos
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
